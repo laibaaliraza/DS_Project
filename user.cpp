bb78260cccf97aab30b3fe1d99891c980c12c070
Taha Kamran <codextaha@gmail.com>
2025-10-21 00:54:03 +0500

    Code Analysis
    After analysing the code, I found a little changes to do, and some suggestion mentioned as comments.

--- user.cpp
+++ user.cpp
@@ -4,6 +4,14 @@ using namespace std;
 User::User() {
     head = nullptr;
 }
+User::~User() {
+    Node* temp = head;
+    while (temp != nullptr) {
+        Node* next = temp->next;
+        delete temp;
+        temp = next;
+    }
+}
 bool User::addusr(string name, string email, string pswd, string role) { //this will add the user in linked list
     Node* new_node = new Node(name, email, pswd, role);
     if (head == nullptr) {
@@ -11,7 +19,15 @@ bool User::addusr(string name, string email, string pswd, string role) { //this 
         return true;
     }
     Node* temp = head;
-    while (temp->next != nullptr) {
+    while (temp != nullptr) {
+        // Review: 
+        // 1.  A compact check to verify unique user. 
+        // 2.  What does role specify here, if it meant Student, Teacher, Admin then 
+        //     will not it make only 3 nodes, cuz after all it the check will prevent
+        //     repitition. If it meant for something else then I would prefer to get insights.
+        // 3.  How about making assigning a hidden unique ID to each user, so no worry of repitition. BUT
+        //     BUT it is just an idea yet. Don't know yet whether in future we need to again use name
+        //     at some places. 
         if (temp->name == new_node->name && temp->role == new_node->role) {
             cout << "\nThis user is already in the linked list: (Same name, Same role)";
             delete new_node;
@@ -26,7 +42,7 @@ bool User::addusr(string name, string email, string pswd, string role) { //this 
 bool User::removeusr(string& name) { //delete the user from the list
     Node* temp = head;
     if (head == nullptr) {
-        cout << "\nUnderflow condition!";
+        cout << "\nList is empty. No user to remove.";
         return false;
     }
     while (temp != nullptr) {
@@ -77,4 +93,5 @@ void User::display() {
         cout << "\n name: " << temp->name << "\nemail id: " << temp->email << "\nRole: " << temp->role;
         temp = temp->next;
     }
+
 }
